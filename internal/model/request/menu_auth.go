719ab2894d8e06a16c34e6f57468a978dea3fcb9
Wwhds_one <a1605691832@163.com>
2025-06-01 21:01:27 +0800

    fix: 允许权限添加时ID值为空

--- internal/model/request/menu_auth.go
+++ internal/model/request/menu_auth.go
@@ -3,10 +3,10 @@ package request
 import "Art-Design-Backend/internal/model/base"
 
 type MenuAuth struct {
-	ID       base.LongStringID `json:"id" label:"菜单ID"`
-	ParentID base.LongStringID `json:"parentID" binding:"required" label:"父菜单ID"`
-	Title    string            `json:"title" binding:"required" label:"菜单标题"`
-	AuthCode string            `json:"authCode" binding:"required" label:"权限编码"`
-	Sort     int               `json:"sort" binding:"required" label:"排序"`
-	Type     int8              `json:"type" binding:"required" label:"菜单类型"`
+	ID       *base.LongStringID `json:"id" label:"菜单ID"`
+	ParentID base.LongStringID  `json:"parentID" binding:"required" label:"父菜单ID"`
+	Title    string             `json:"title" binding:"required" label:"菜单标题"`
+	AuthCode string             `json:"authCode" binding:"required" label:"权限编码"`
+	Sort     int                `json:"sort" binding:"required" label:"排序"`
+	Type     int8               `json:"type" binding:"required" label:"菜单类型"`
 }
